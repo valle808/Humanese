0914a502b7cd6a57402abd307c78015d6321fc93
GIO V <sergiofishemoji@gmail.com>
2026-02-27 02:57:39 -1000

    Fix crossOrigin attribute in AdSense script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <script src="https://cdn.tailwindcss.com"></script>
         {/* Google AdSense Script Injected System-Wide */}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8867340586657793"
-          crossorigin="anonymous"></script>
+        crossOrigin="anonymous"></script
         <meta name="google-adsense-account" content="ca-pub-8867340586657793" />
         <meta name="theme-color" content="#0b0f19" />
       </head>
